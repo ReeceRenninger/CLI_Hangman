11642184dfc94678bf354003a31923b4cd11b652
ReeceRenninger <renningerreece@gmail.com>
2024-01-12 09:05:47 -0800

    draw hangman function, word guess, player input, game over, main game loop, just need a game over portion in the game loop to complete

--- hangman.py
+++ hangman.py
@@ -26,3 +26,165 @@ def _validate_input(player_input, guessed_letters):
         # making sure guess is NOT a repeat letter
         and player_input not in guessed_letters
     )
+
+# create a set to track guessed letters
+def join_guessed_letters(guessed_letters):
+    # sorted with alphabetize the guessed letters to be returned
+    return " ".join(sorted(guessed_letters))
+
+# display the word to show the player
+def build_guessed_word(target_word, guessed_letters):
+    current_letters = []
+    # build current_letters list with for loop
+    for letter in target_word:
+        if letter in guessed_letters:
+            current_letters.append(letter)
+        else:
+            current_letters.append("_")
+    return " ".join(current_letters)
+
+# draw the hang man with raw strings
+# hangman.py
+# ...
+
+def draw_hanged_man(wrong_guesses):
+    hanged_man = [
+        r"""
+  -----
+  |   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+      |
+      |
+      |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+  |   |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ |   |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+      |
+      |
+      |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/     |
+|     |
+      |
+-------
+""",
+        r"""
+  -----
+  |   |
+  O   |
+ ---  |
+/ | \ |
+  |   |
+ ---  |
+/   \ |
+|   | |
+      |
+-------
+""",
+    ]
+
+    print(hanged_man[wrong_guesses])
+
+
+# need a variable to check for max incorrect guesses allowed
+MAX_GUESSES = 6 
+
+# game over function to check for max guesses reached or word was guessed correctly
+def game_over(wrong_guesses, target_word, guessed_letters):
+    if wrong_guesses == MAX_GUESSES:
+        return True
+    # sets are unique so using the <= compartor checks every item left hand set is a member of the right hand set, if all are the same, the word has been guessed
+    if set(target_word) <= guessed_letters:
+        return True
+    return False
+
+#!! START GAME AREA
+if __name__ == "__main__":
+    # grab word from word list
+    target_word = select_word()
+    # create a new set of guessed letters for player
+    guessed_letters = set() 
+    guessed_word = build_guessed_word(target_word, guessed_letters)
+    # set wrong guesses to 0 for game over function
+    wrong_guesses = 0
+    print("Welcome to CLI Hangman, Good Luck!")
+
+    #!! Main Game Loop
+    while not game_over(wrong_guesses, target_word, guessed_letters):
+        draw_hanged_man(wrong_guesses)
+        print(f"YOur word is: {guessed_word}")
+        print(
+            "Current Guessed Letters: "
+            f"{join_guessed_letters(guessed_letters)}\n"
+        )
+
+        player_guess = get_player_input(guessed_letters)
+        if player_guess in target_word:
+            print("Nice Guess!")
+        else:
+            print("Uh Oh, that one isn't there.")
+            wrong_guesses += 1
+        
+        guessed_letters.add(player_guess)
+        guessed_word = build_guessed_word(target_word, guessed_letters)
+
+        
