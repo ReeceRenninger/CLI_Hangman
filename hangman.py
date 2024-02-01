fc80e285138e209b130ac41b727538e9cd21f33a
ReeceRenninger <renningerreece@gmail.com>
2024-02-01 08:06:36 -0800

    game play loop was failing and sending final word on every guess, fixed this and cleaned up image of hangman printing for final message

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 from random import choice
 import string
 
+
 # create a select word function to grab a word from our words.txt file
 def select_word():
     # creating a words variable to have readlines() iterate over
@@ -8,14 +9,16 @@ def select_word():
         word_list = words.readlines()
     return choice(word_list).strip()
 
+
 # grab player input function
 def get_player_input(guessed_letters):
     while True:
-        #normalize player input with .lower()
+        # normalize player input with .lower()
         player_input = input("Guess a letter: ").lower()
         if _validate_input(player_input, guessed_letters):
             return player_input
-        
+
+
 # validator function to ensure player is inputting correct guess types
 def _validate_input(player_input, guessed_letters):
     return (
@@ -27,10 +30,12 @@ def _validate_input(player_input, guessed_letters):
         and player_input not in guessed_letters
     )
 
+
 # create a set to track guessed letters
 def join_guessed_letters(guessed_letters):
     # sorted with alphabetize the guessed letters to be returned
     return " ".join(sorted(guessed_letters))
+
 
 # display the word to show the player
 def build_guessed_word(target_word, guessed_letters):
@@ -43,9 +48,11 @@ def build_guessed_word(target_word, guessed_letters):
             current_letters.append("_")
     return " ".join(current_letters)
 
+
 # draw the hang man with raw strings
 # hangman.py
 # ...
+
 
 def draw_hanged_man(wrong_guesses):
     hanged_man = [
@@ -146,7 +153,8 @@ def draw_hanged_man(wrong_guesses):
 
 
 # need a variable to check for max incorrect guesses allowed
-MAX_GUESSES = 6 
+MAX_GUESSES = 6
+
 
 # game over function to check for max guesses reached or word was guessed correctly
 def game_over(wrong_guesses, target_word, guessed_letters):
@@ -155,14 +163,16 @@ def game_over(wrong_guesses, target_word, guessed_letters):
     # sets are unique so using the <= compartor checks every item left hand set is a member of the right hand set, if all are the same, the word has been guessed
     if set(target_word) <= guessed_letters:
         return True
-    return False
+    else:
+        return False
+
 
 #!! START GAME AREA
 if __name__ == "__main__":
     # grab word from word list
     target_word = select_word()
     # create a new set of guessed letters for player
-    guessed_letters = set() 
+    guessed_letters = set()
     guessed_word = build_guessed_word(target_word, guessed_letters)
     # set wrong guesses to 0 for game over function
     wrong_guesses = 0
@@ -172,10 +182,7 @@ if __name__ == "__main__":
     while not game_over(wrong_guesses, target_word, guessed_letters):
         draw_hanged_man(wrong_guesses)
         print(f"Your word is: {guessed_word}")
-        print(
-            "Current Guessed Letters: "
-            f"{join_guessed_letters(guessed_letters)}\n"
-        )
+        print("Current Guessed Letters: " f"{join_guessed_letters(guessed_letters)}\n")
 
         player_guess = get_player_input(guessed_letters)
         if player_guess in target_word:
@@ -183,15 +190,17 @@ if __name__ == "__main__":
         else:
             print("Uh Oh, that one isn't there.")
             wrong_guesses += 1
-        
+
         guessed_letters.add(player_guess)
         guessed_word = build_guessed_word(target_word, guessed_letters)
 
         # Game over trigger
-        draw_hanged_man(wrong_guesses)
         if wrong_guesses == MAX_GUESSES:
-            print("Better luck next time!")
-        else:
+            draw_hanged_man(wrong_guesses)
+            print(f"Better luck next time, your word was: {target_word}")
+        elif target_word == guessed_letters:
             print("You did it! Congratulations!")
-        print(f"Better luck next time, your word was: {target_word}")
+
+
+
 
